e302a005b3e9eefd8b8eabdbefd8f7e431110935
Maciej Tubek <maciej.tubek@ex7.pl>
2021-11-21 10:50:18 +0100

    Move creating and checkout of branch to libgit2sharp

--- FastGitBranchX64/Commands/CreateBranch.cs
+++ FastGitBranchX64/Commands/CreateBranch.cs
@@ -3,19 +3,23 @@ using Microsoft.VisualStudio.Shell.Interop;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Windows;
+using Commands = LibGit2Sharp.Commands;
 
 namespace FastGitBranchX64
 {
     [Command(PackageIds.CreateBranch)]
     internal sealed class CreateBranch : BaseCommand<CreateBranch>
     {
-        Guid id = new("e6afbe38-c42e-402c-866f-b6a612be4245");
-        Repository gitRepository;
+        private Guid id = new("e6afbe38-c42e-402c-866f-b6a612be4245");
+        private Repository gitRepository;
+
         protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
         {
             var solutionPath = await GetSolutionPathAsync();
             gitRepository = new Repository(solutionPath);
+
             #region definiowanie outputu
+
             var outputwindow = await VS.Services.GetOutputWindowAsync();
             await Microsoft.VisualStudio.Shell.ThreadHelper.JoinableTaskFactory.RunAsync(async delegate
             {
@@ -25,9 +29,8 @@ namespace FastGitBranchX64
                     outputwindow.CreatePane(id, "Fast Branch", 1, 0);
                 }
             });
-            #endregion
 
-
+            #endregion definiowanie outputu
 
             var gitBranchUC = new FastGitBranchControl(gitRepository);
             System.Windows.Window window = new System.Windows.Window
@@ -42,24 +45,22 @@ namespace FastGitBranchX64
             };
             window.ShowDialog();
 
-            if(gitBranchUC.CreateBranch)
+            if (gitBranchUC.CreateBranch)
             {
-                
-
                 string gitCommand = "git";
                 string gitArgumentStart = $@"-C {solutionPath}";
 
                 await VS.StatusBar.StartAnimationAsync(StatusAnimation.Sync);
-                await VS.StatusBar.ShowMessageAsync("Creating branch...");
+                await VS.StatusBar.ShowMessageAsync($"Creating branch {gitBranchUC.BranchName}");
+                WriteToOutputWindow($"Creating branch {gitBranchUC.BranchName}");
+                var branch = gitRepository.CreateBranch(gitBranchUC.BranchName);
                 if (gitBranchUC.Checkout)
                 {
-                    RunGitCommand(gitCommand, $@"{gitArgumentStart} checkout -b {gitBranchUC.BranchName}");
+                    WriteToOutputWindow($"Checkout branch {gitBranchUC.BranchName}");
+                    Commands.Checkout(gitRepository, branch);
                 }
-                else
-                {
-                    RunGitCommand(gitCommand, $@"{gitArgumentStart} branch {gitBranchUC.BranchName}");
-                }
-                    
+                WriteToOutputWindow($"Current HEAD => {gitRepository.Head.Reference.TargetIdentifier}");
+
                 await VS.StatusBar.EndAnimationAsync(StatusAnimation.Sync);
 
                 await VS.StatusBar.ClearAsync();
@@ -83,20 +84,21 @@ namespace FastGitBranchX64
         private async Task<string> GetSolutionPathAsync()
         {
             var solucja = await VS.Solutions.GetCurrentSolutionAsync();
-            if(solucja !=null && solucja.FullPath!=null)
+            if (solucja != null && solucja.FullPath != null)
             {
                 var solucjaExp = solucja.FullPath.Split(new char[] { '\\' }, System.StringSplitOptions.RemoveEmptyEntries);
                 var solutionPath = string.Join("\\", solucjaExp, 0, solucjaExp.Length - 1);
                 return solutionPath;
             }
-            
+
             return string.Empty;
         }
 
-        async void WriteToOutputWindow(string input)
+        private async void WriteToOutputWindow(string input)
         {
             var outputwindow = await VS.Services.GetOutputWindowAsync();
-            await Microsoft.VisualStudio.Shell.ThreadHelper.JoinableTaskFactory.RunAsync(async delegate {
+            await Microsoft.VisualStudio.Shell.ThreadHelper.JoinableTaskFactory.RunAsync(async delegate
+            {
                 await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
                 outputwindow.GetPane(id, out IVsOutputWindowPane pane);
                 if (input != null)
