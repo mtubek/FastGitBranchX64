0917c9322817f74dd04448047e8dbb5a29f596e2
Maciej Tubek <maciej.tubek@ex7.pl>
2021-11-21 22:22:00 +0100

    Add CLickUp task BO

---
+++ FastGitBranchX64/BO/ClickUP/Status.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace FastGitBranchX64.BO.ClickUP
+{
+    public class Status
+    {
+        public string id { get; set; }
+        public string status { get; set; }
+        public string color { get; set; }
+        public int orderindex { get; set; }
+        public string type { get; set; }
+    }
+}
---
+++ FastGitBranchX64/BO/ClickUP/Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastGitBranchX64.BO.ClickUP
+{
+    public class Task
+    {
+        public string id { get; set; }
+        public string custom_id { get; set; }
+        public string name { get; set; }
+        public string text_content { get; set; }
+        public string description { get; set; }
+        public Status status { get; set; }
+        
+    }
+}
